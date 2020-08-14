34a8950d71c72ac431eefec3ddfb95ca6d3b66f0
Azur冰弦 <973562770@qq.com>
2020-08-14 22:52:05 +0800

    Update 1.16.2 flat.sh

--- Scripts/1.16.2 flat.sh
+++ Scripts/1.16.2 flat.sh
@@ -1,1 +1,2 @@
-cd G:\_MC\_Server\_AzurCraft\1.16.2 flat && java -jar -Xms6g -Xmx6g fabric-server-launch.jar --nogui
+#!/bin/sh
+cd /mnt/g/_MC/_Server/_AzurCraft/1.16.2\ flat && java -jar -Xms6g -Xmx6g fabric-server-launch.jar --nogui
